537db12d4b13d737dfe2b4da5c70e3da20f9405d
Kostya <kostya173russ@gmail.com>
2023-05-21 15:45:14 +0300

    сделано 4-ое задание

---
+++ HomeworkTask4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine());
+int z = 1;
+int result = 0;
+Console.WriteLine("Все чётные числа от 1 до введеного числа А");
+while (z <= a)
+{
+    result = z % 2;
+    if (result == 0) Console.WriteLine(z);
+  z++;}
+  
+
+    
